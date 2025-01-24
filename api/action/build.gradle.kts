8497182cb42f5e0655accae0f067e650c7a4ebc3
dkinging <1520622465@qq.com>
2025-01-25 00:34:47 +0800

    Command Action Support Folia

--- api/action/build.gradle.kts
+++ api/action/build.gradle.kts
@@ -2,6 +2,11 @@ dependencies {
     api(project(":common"))
 
     compileOnly("net.md-5:bungeecord-chat:1.18-R0.1-SNAPSHOT")
+
+
+    // Folia
+    compileOnly("io.paper:folia-api:1.20.4")
+
 }
 
 taboolib { subproject = true }
--- api/action/src/main/kotlin/trplugins/menu/api/action/impl/send/Command.kt
+++ api/action/src/main/kotlin/trplugins/menu/api/action/impl/send/Command.kt
@@ -2,9 +2,11 @@ package trplugins.menu.api.action.impl.send
 
 import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.function.submit
+import taboolib.platform.Folia
 import trplugins.menu.api.action.ActionHandle
 import trplugins.menu.api.action.base.ActionBase
 import trplugins.menu.api.action.base.ActionContents
+import trplugins.menu.api.utils.FoliaUtil
 
 /**
  * TrMenu
@@ -19,8 +21,13 @@ class Command(handle: ActionHandle) : ActionBase(handle) {
 
     override fun onExecute(contents: ActionContents, player: ProxyPlayer, placeholderPlayer: ProxyPlayer) {
         placeholderPlayer.parseContentSplited(contents.stringContent(), ";").forEach {
-            submit(async = false) {
-                player.performCommand(it)
+            // Folia 必须使用实体调度器 暂时不做优雅的兼容性处理 后期其他功能需要单独处理Folia时再修改
+            if (Folia.isFolia) {
+                FoliaUtil.invokeCommandWithEntityScheduler(player,it)
+            }else{
+                submit(async = false) {
+                    player.performCommand(it)
+                }
             }
         }
     }
