1739c45ed38ead081e3a0cb3704a2e5e31a3ca09
嘿鹰 <hhhhhy1747@outlook.com>
2025-11-10 18:05:39 +0800

    Bump TabooLib and project version
    Updated TabooLib dependency to 6.2.4-621517d

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ subprojects {
             disableOnSkippedVersion = false
         }
         version {
-            taboolib = "6.2.3-d4a5f0ea"
+            taboolib = "6.2.4-621517d"
             coroutines = null
         }
     }
