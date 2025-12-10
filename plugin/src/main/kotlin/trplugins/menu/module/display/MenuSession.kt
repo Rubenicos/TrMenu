a222bd20aec9b833790cef50da27f77443a21f7b
嘿鹰 <hhhhhy1747@outlook.com>
2025-12-10 11:09:31 +0800

    feat(settings): 支持在 Shortcuts.*-Click-Player 不打开菜单情况下对 {0} 解析目标玩家名

--- plugin/src/main/kotlin/trplugins/menu/module/display/MenuSession.kt
+++ plugin/src/main/kotlin/trplugins/menu/module/display/MenuSession.kt
@@ -144,7 +144,7 @@ class MenuSession(
             }
         }
         val content =
-            (if (preColor) funced else funced.colored().parseRainbow().parseGradients()).replaceWithOrder(*arguments)
+            (if (preColor) funced else funced.colored().parseRainbow().parseGradients()).replaceWithOrder(*arguments).replaceWithOrder(*implicitArguments)
         val papi = content.replacePlaceholder(placeholderPlayer)
 
         return if (preColor) papi else papi.colored().parseRainbow().parseGradients()
